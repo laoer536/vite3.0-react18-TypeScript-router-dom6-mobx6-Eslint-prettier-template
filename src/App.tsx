0a843c7a26be5937e809030e37b97f966a3845ba
laoer536 <1547698569@qq.com>
2023-02-08 00:28:22 +0800

    feat: code-style-lint-react-ts引入

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import MyRoutes from '@/router'
-import { useVcosole } from './hooks/useVconsole'
+import { useVcosole } from '@hooks/useVconsole'
 // 这个是全局的页面 还可以做一些其他的操作
 
 export default function App() {
