b16f963010a832058945e3cd718bafa9297f5f4b
laoer536 <1547698569@qq.com>
2024-01-20 23:10:28 +0800

    feat: about page add 'back'

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -3,14 +3,21 @@ import { useUserStore } from '@store/user'
 import { motion } from 'framer-motion'
 
 function About() {
+  const navigate = useNavigate()
   const [pageTitle] = useState('laoer536-About Page')
   const { num, changeNum } = useUserStore()
+
   return (
     <div>
       <h1>{pageTitle}</h1>
       <h2>userSore.num:{num}</h2>
       <motion.button {...button} onClick={changeNum}>
         click on the store provided by zustand to change the number
+      </motion.button>
+      <br />
+      <br />
+      <motion.button {...button} onClick={() => navigate(-1)}>
+        back
       </motion.button>
     </div>
   )
