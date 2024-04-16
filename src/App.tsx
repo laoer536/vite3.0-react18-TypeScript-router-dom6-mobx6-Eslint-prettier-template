c7cfd73ffe6cb4f9d3b512d28462533fc36b838e
LiuJie <1547698569@qq.com>
2024-04-16 14:46:27 +0800

    perf: useVcosole

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,13 @@ import MyRoutes from '@/router'
 // 这个是全局的页面 还可以做一些其他的操作
 
 export default function App() {
-  useVcosole()
+  const [vc] = useVcosole()
+  useEffect(() => {
+    console.log('VConsole ?', vc)
+    if (vc) {
+      vc.show()
+    }
+  }, [])
   return (
     <div>
       <MyRoutes />
--- src/hooks/useVconsole.ts
+++ src/hooks/useVconsole.ts
@@ -1,6 +1,8 @@
 import VConsole from 'vconsole'
 export function useVcosole() {
+  const vc = useRef<VConsole | null>(null)
   if (location.href.includes('#vc')) {
-    new VConsole({ theme: 'dark', maxLogNumber: 1000 })
+    vc.current = new VConsole({ theme: 'dark', maxLogNumber: 1000 })
   }
+  return [vc.current] as const
 }
