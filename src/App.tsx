010c02ef81ed22e7902b5bd82bdee59a6a666d4d
LiuJie <1547698569@qq.com>
2024-11-02 22:35:32 +0800

    feat: event support

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import WelcomeModal from '@components/Welcome'
 import { useVcosole } from '@hooks/useVconsole'
 
 import MyRoutes from '@/router'
-import WelcomeModal from '@components/Welcome'
 // 这个是全局的页面 还可以做一些其他的操作
 
 export default function App() {
